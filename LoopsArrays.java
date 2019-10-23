88e9162c2d539350bfac0f38e69653f146d34fbe
afroditi-1995 <56820747+afroditi-1995@users.noreply.github.com>
2019-10-23 21:24:48 +0300

    Add files via upload

---
+++ LoopsArrays.java
@@ -0,0 +1,12 @@
+package loops.arrays;
+
+public class LoopsArrays {
+
+    public static void main(String[] args) {
+        
+        String[] bestFriends = {"Stella", "Elli", "Thodoris", "Giannis"};   
+        for(int i=0;i<=3;i++)
+            System.out.println(bestFriends[i]);
+    }
+    
+}
