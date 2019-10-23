4648391132ff34d463694c68f4a603693896ebb2
afroditi-1995 <56820747+afroditi-1995@users.noreply.github.com>
2019-10-23 19:14:39 +0300

    Add files via upload

---
+++ GoodMorning.java
@@ -0,0 +1,12 @@
+package goodmorning;
+
+public class GoodMorning {
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is 22nd of October");
+        System.out.println("Winter is coming");
+        
+    }
+    
+}
